70c92e68152757d216118913a7e1b993b56f31db
KareemAlaa2001 <kareemalaa2001@gmail.com>
2022-07-27 12:09:34 +0100

    made the test function minibatched as well

--- trainTestHeteroMinibatched.py
+++ trainTestHeteroMinibatched.py
@@ -13,8 +13,7 @@ from tqdm import tqdm
 from torch import nn, svd
 from utils import accuracy,init_weights
 
-from sklearn.metrics import f1_score
-from sklearn.metrics import matthews_corrcoef, precision_score, recall_score, roc_auc_score, precision_recall_curve, confusion_matrix
+from sklearn.metrics import f1_score, matthews_corrcoef, precision_score, recall_score, roc_auc_score, precision_recall_curve, confusion_matrix
 import wandb
 
 
@@ -70,6 +69,66 @@ def test_minibatched(loader, model, loss, device):
 
     return test_loss, test_acc
 
+def test_minibatched_with_metrics(loader, model, loss, device, **metrics):
+    model.eval()
+    total_examples = total_loss = total_acc = 0
+
+    metric_totals = {metric: 0.0 for metric in metrics}
+
+    total_conf_matrix = torch.zeros(2, 2)
+
+    for data in loader:
+        data = data.to(device, 'edge_index')
+
+        batch_size = data['user'].batch_size
+        output = model(data)
+
+        loss_batch = loss(output[:batch_size], data['user'].y[:batch_size])
+        acc_batch = accuracy(output[:batch_size],  data['user'].y[:batch_size])
+
+        total_loss += loss_batch * batch_size
+        total_acc += acc_batch * batch_size
+
+        total_examples += batch_size
+
+        y_pred = output[:batch_size].max(1)[1].detach().numpy()
+        y_true = data['user'].y[:batch_size].detach().numpy()
+
+
+
+        for metric in metrics:
+            try:
+                metric_totals[metric] += metrics[metric](y_pred, y_true) * batch_size
+            except ValueError:
+                print("Got the valueerror from a batch not containing both classes, continuing..")
+                continue
+        # f1_batch = f1_score(y_true, y_pred, average='macro')
+        # mcc_batch = matthews_corrcoef(y_true, y_pred)
+        # precision_batch = precision_score(y_true, y_pred, average='macro')
+        # recall_batch = recall_score(y_true, y_pred, average='macro')
+        # roc_auc_batch = roc_auc_score(y_true, y_pred, average='macro')
+
+        # total_f1 += f1_batch * batch_size
+        # total_mcc += mcc_batch * batch_size
+        # total_precision += precision_batch * batch_size
+        # total_recall += recall_batch * batch_size
+        # total_roc_auc += roc_auc_batch * batch_size
+
+        conf_matrix_batch = confusion_matrix(y_true, y_pred)
+        total_conf_matrix += conf_matrix_batch
+
+    test_loss = total_loss / total_examples
+    test_acc = total_acc / total_examples
+
+    results = {metric: metric_totals[metric] / total_examples for metric in metrics}
+    results['conf_matrix'] = total_conf_matrix
+    results['loss_test'] = test_loss
+    results['acc_test'] = test_acc
+
+    return results
+
+
+
 
 def trainTestHeteroMinibatched(embedding_size = 128, dropout = 0.3, lr = 1e-3, weight_decay = 5e-3, svdComponents = 100, thirds = False, epochs = 100, extraLayer=True, numHanLayers = 2, neighboursPerNode = 50, batch_size = 256):
     wandb.init(project="test-project", entity="graphbois")
@@ -80,17 +139,19 @@ def trainTestHeteroMinibatched(embedding_size = 128, dropout = 0.3, lr = 1e-3, w
     print("importing the dataset...")
 
     dataset = TwibotSmallEdgeHetero(device=device,process=True,save=True,dev=False, svdComponents=svdComponents)
-    dataset = initializeHeteroTwibot(dataset)
+    dataset = initializeHeteroTwibot(dataset).to(device, 'x', 'y')
 
-    kwargs = {'num_workers': min(torch.cuda.device_count(),4) if torch.cuda.device_count() > 0 else 1, 'persistent_workers': True, 'batch_size': batch_size}
+    # min(torch.cuda.device_count(),4) if torch.cuda.device_count() > 0 else 1
+    kwargs = {'num_workers': 4, 'persistent_workers': True, 'batch_size': batch_size}
 
     train_loader = NeighborLoader(dataset, num_neighbors=[neighboursPerNode] * numHanLayers,shuffle=False, input_nodes=('user',dataset['user'].train_mask), **kwargs)
     val_loader = NeighborLoader(dataset, num_neighbors=[neighboursPerNode] * numHanLayers,shuffle=False, input_nodes=('user',dataset['user'].val_mask), **kwargs)
-
+    test_loader = NeighborLoader(dataset, num_neighbors=[neighboursPerNode] * numHanLayers,shuffle=False, input_nodes=('user',dataset['user'].test_mask), **kwargs)
 
 
     # model = TweetAugmentedHAN2ExtraLayer(embedding_dimension=embedding_size,des_size=svdComponents, tweet_size=svdComponents, metadata=dataset.metadata()).to(device)
     model = TweetAugHANConfigurable(embedding_dimension=embedding_size,des_size=svdComponents, tweet_size=svdComponents, metadata=dataset.metadata(), extraLayer=extraLayer,numHanLayers=numHanLayers).to(device)
+
     wandb.config.update({
     "model_name": model.__class__.__name__,
     "dataset": dataset.__class__.__name__,
@@ -130,9 +191,15 @@ def trainTestHeteroMinibatched(embedding_size = 128, dropout = 0.3, lr = 1e-3, w
                 'acc_val: {:.4f}'.format(val_loss.item()),
                 'loss_val: {:.4f}'.format(val_acc.item()))
 
-    acc_test,loss_test,f1, roc_auc = test(model, dataset, loss)
+    metrics = {'f1_score': f1_score, 'mcc': matthews_corrcoef, 'prec': precision_score, 'recall': recall_score, 'roc_auc': roc_auc_score}
+    
 
-    return roc_auc
+    results = test_minibatched_with_metrics(test_loader, model, loss, device, **metrics)
+    wandb.log(results)
+
+    # acc_test,loss_test,f1, roc_auc = test(model, dataset, loss)
+
+    return results
 
 if __name__ == '__main__':
     wandb.init(project="test-project", entity="graphbois")
@@ -152,7 +219,7 @@ if __name__ == '__main__':
     weight_decay = 5e-3
     svdComponents = 200
     thirds = True
-    epochs = 50
+    epochs = 1
     extraLayer = True
     numHanLayers = 4
     neighboursPerNode = 200
