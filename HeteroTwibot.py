955d8f351c800d6b17b41819c1a42083415b9250
KareemAlaa2001 <kareemalaa2001@gmail.com>
2022-08-07 14:44:50 +0100

    uncommented HeteroTwibot class to resolve import errors

--- HeteroTwibot.py
+++ HeteroTwibot.py
@@ -4,34 +4,34 @@ import torch
 from torch_geometric.data import HeteroData
 
 
-# class HeteroTwibot():
-#     def __init__(self, edgeHetero: TwibotSmallEdgeHetero):
-#         des_tensor,tweets_tensor,num_prop,category_prop,edge_index,edge_type,labels,train_idx,val_idx,test_idx=edgeHetero.dataloader()
+class HeteroTwibot():
+    def __init__(self, edgeHetero: TwibotSmallEdgeHetero):
+        des_tensor,tweets_tensor,num_prop,category_prop,edge_index,edge_type,labels,train_idx,val_idx,test_idx=edgeHetero.dataloader()
 
-#         self.data = HeteroData()
+        self.data = HeteroData()
 
-#         self.data['user'].des = des_tensor
-#         self.data['user'].cat = category_prop
-#         self.data['user'].num = num_prop
-#         # self.data['user'].x = None
-#         self.data['user'].x = torch.cat((des_tensor, category_prop, num_prop), dim=1)
-#         self.data['user'].y = labels
-#         self.data['user'].train_idx = train_idx
-#         self.data['user'].val_idx = val_idx
-#         self.data['user'].test_idx = test_idx
-#         self.data['tweet'].x = tweets_tensor
+        self.data['user'].des = des_tensor
+        self.data['user'].cat = category_prop
+        self.data['user'].num = num_prop
+        # self.data['user'].x = None
+        self.data['user'].x = torch.cat((des_tensor, category_prop, num_prop), dim=1)
+        self.data['user'].y = labels
+        self.data['user'].train_idx = train_idx
+        self.data['user'].val_idx = val_idx
+        self.data['user'].test_idx = test_idx
+        self.data['tweet'].x = tweets_tensor
 
-#         self.data['user', 'following', 'user'].edge_index = edge_index[:, (edge_type == 0).nonzero().squeeze()]
-#         self.data['user', 'followedBy', 'user'].edge_index = edge_index[:, (edge_type == 1).nonzero().squeeze()]
-#         self.data['tweet', 'mentions', 'user'].edge_index = edge_index[:, (edge_type == 2).nonzero().squeeze()]
-#         self.data['tweet', 'mentions', 'user'].edge_index[0] = self.data['tweet', 'mentions', 'user'].edge_index[0] - self.data['user'].x.shape[0]
-#         self.data['tweet', 'retweets', 'user'].edge_index = edge_index[:, (edge_type == 3).nonzero().squeeze()]
-#         self.data['tweet', 'retweets', 'user'].edge_index[0] = self.data['tweet', 'retweets', 'user'].edge_index[0] - self.data['user'].x.shape[0]
-#         self.data['user','writes', 'tweet'].edge_index = edge_index[:, (edge_type == 4).nonzero().squeeze()]
-#         self.data['user','writes', 'tweet'].edge_index[1] = self.data['user','writes', 'tweet'].edge_index[1] - self.data['user'].x.shape[0]
+        self.data['user', 'following', 'user'].edge_index = edge_index[:, (edge_type == 0).nonzero().squeeze()]
+        self.data['user', 'followedBy', 'user'].edge_index = edge_index[:, (edge_type == 1).nonzero().squeeze()]
+        self.data['tweet', 'mentions', 'user'].edge_index = edge_index[:, (edge_type == 2).nonzero().squeeze()]
+        self.data['tweet', 'mentions', 'user'].edge_index[0] = self.data['tweet', 'mentions', 'user'].edge_index[0] - self.data['user'].x.shape[0]
+        self.data['tweet', 'retweets', 'user'].edge_index = edge_index[:, (edge_type == 3).nonzero().squeeze()]
+        self.data['tweet', 'retweets', 'user'].edge_index[0] = self.data['tweet', 'retweets', 'user'].edge_index[0] - self.data['user'].x.shape[0]
+        self.data['user','writes', 'tweet'].edge_index = edge_index[:, (edge_type == 4).nonzero().squeeze()]
+        self.data['user','writes', 'tweet'].edge_index[1] = self.data['user','writes', 'tweet'].edge_index[1] - self.data['user'].x.shape[0]
 
-#     # def dataloader():
-#     #     return
+    # def dataloader():
+    #     return
 
 def initializeHeteroAugTwibot(edgeHetero: TwibotSmallEdgeHetero) -> HeteroData:
     des_tensor,tweets_tensor,num_prop,category_prop,edge_index,edge_type,labels,train_idx,val_idx,test_idx=edgeHetero.dataloader()
