ab5f2a19c7221f50c8dd6ebb03923cbcce2b8b15
KareemAlaa2001 <kareemalaa2001@gmail.com>
2022-08-17 21:23:04 +0100

    added bash scripts for optimising han on user only graph

---
+++ optimiseHANUserOnly.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N optimiseHANUserOnly
+#$ -cwd
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=48G
+#  These options are:
+#  job name: -N
+#  use the current working directory: -cwd
+#  runtime limit of 24 hours: -l h_rt
+#  memory limit of 48 Gbyte (dissa ram heavy boi): -l h_vmem
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# load anaconda
+module load anaconda
+
+# Activate botRGCN conda environment
+source activate botRGCN
+
+# Run the wandb agent for hyperparameter sweeping
+wandb agent graphbois/test-project/dv5257nw
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python crossValTrainTest.py $1 $2 --dataset_variant $3 
